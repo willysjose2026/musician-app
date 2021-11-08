36aed51948a5ac11371ed93bc63f08318c08bc5f
willysjose2026 <50811468+willysjose2026@users.noreply.github.com>
2021-11-07 20:41:53 -0400

    change in route

--- routes/musician.js
+++ routes/musician.js
@@ -5,7 +5,7 @@ const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
 // healthcheck
-router.get('/health', (req, res) => {
+router.get('/admin', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
